3dde553ca228d6d4e334c4fdd233d85560a346f2
xautanyang <xautanyang@163.com>
2016-07-20 16:38:05 +0800

    create CashRegisterTest

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -8,9 +8,13 @@ import static org.junit.Assert.*;
  * Created by anyang on 2016/7/20.
  */
 public class CashRegisterTest {
+
+    CashRegister cashRegister;
+
     @Before
     public void setUp() throws Exception {
-
+        cashRegister = new CashRegister();
     }
+
 
 }
