f543154f36fc7747c3b5af0443ab59fb41039b70
xautanyang <xautanyang@163.com>
2016-07-20 16:37:18 +0800

    delete comment

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -1,61 +1,16 @@
-<<<<<<< HEAD
 package cashregister;
 
-import calculator.CalculatorWithBuyTwoGetOneProduct;
-import calculator.CalculatorWithDiscountProduct;
-import calculator.CalculatorWithOriginalPriceProduct;
-import model.Product;
-=======
-<<<<<<< HEAD
-package cashregister;
-
->>>>>>> c6b381d997b674da5b0ecf9ca504e6a6885534c5
 import org.junit.Before;
-import org.junit.Test;
 
-import java.util.HashMap;
-
-<<<<<<< HEAD
-=======
-/**
- * Created by carrie on 2016/7/20.
- */
-public class CashRegisterTest {
-    CashRegister cashRegister;
-    @Before
-    public void setUp() throws Exception {
-
-    }
-
-    @Test
-    public void testCashRegister() throws Exception {
-        cashRegister = new CashRegister();
-
-
-    }
-}
-=======
->>>>>>> c6b381d997b674da5b0ecf9ca504e6a6885534c5
 import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 /**
  * Created by anyang on 2016/7/20.
  */
 public class CashRegisterTest {
-
-<<<<<<< HEAD
-    CashRegister cashRegister;
-
     @Before
     public void setUp() throws Exception {
-        cashRegister = new CashRegister();
+
     }
 
-
 }
-=======
-}
->>>>>>> f5fc2e98582234f445e47c44a8ea2e5244b02c2d
->>>>>>> c6b381d997b674da5b0ecf9ca504e6a6885534c5
