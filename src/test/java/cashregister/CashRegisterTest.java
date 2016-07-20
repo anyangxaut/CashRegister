fe3a4fcdf950f542d04092a083bc2156c71ee018
xautanyang <xautanyang@163.com>
2016-07-20 16:14:03 +0800

    create CashRegisterTest

--- src/test/java/cashregister/CashRegisterTest.java
+++ src/test/java/cashregister/CashRegisterTest.java
@@ -1,8 +1,29 @@
+package cashregister;
+
+import calculator.CalculatorWithBuyTwoGetOneProduct;
+import calculator.CalculatorWithDiscountProduct;
+import calculator.CalculatorWithOriginalPriceProduct;
+import model.Product;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 /**
  * Created by anyang on 2016/7/20.
  */
 public class CashRegisterTest {
+
+    CashRegister cashRegister;
+
+    @Before
+    public void setUp() throws Exception {
+        cashRegister = new CashRegister();
+    }
+
 
 }
