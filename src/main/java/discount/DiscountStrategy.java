977d610ab5c86b022853a162abf64aacd191388d
xautanyang <xautanyang@163.com>
2016-07-20 19:54:06 +0800

    delete comment

--- src/main/java/discount/DiscountStrategy.java
+++ src/main/java/discount/DiscountStrategy.java
@@ -2,9 +2,7 @@ package discount;
 
 import java.util.HashMap;
 
-/**
- * Created by anyang on 2016/7/18.
- */
 public interface DiscountStrategy {
+
     public HashMap<String, String> getDiscount();
 }
