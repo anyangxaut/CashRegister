8e98a9b976842d175c61c084675f5df59f68a96b
xautanyang <xautanyang@163.com>
2016-07-20 15:31:11 +0800

    change test case

--- src/test/java/calculator/CalculatorWithBuyTwoGetOneProductTest.java
+++ src/test/java/calculator/CalculatorWithBuyTwoGetOneProductTest.java
@@ -23,7 +23,7 @@ public class CalculatorWithBuyTwoGetOneProductTest {
     }
 
     @Test
-    public void testCalculatePriceWithSingleProduct() throws Exception {
+    public void testCalculatePriceWithSingleProductAndProductNumberIsNotThreeMultiple() throws Exception {
         HashMap<Product, Integer> buyTwoGetOneProductListWithNumber = new HashMap<Product, Integer>();
         buyTwoGetOneProductListWithNumber.put(createProduct(2.0), 1);
 
@@ -33,39 +33,37 @@ public class CalculatorWithBuyTwoGetOneProductTest {
     }
 
     @Test
-    public void testCalculatePriceWithMultipleProduct() throws Exception {
+    public void testCalculatePriceWithMultipleProductAndProductNumberIsNotThreeMultiple() throws Exception {
         HashMap<Product, Integer> buyTwoGetOneProductListWithNumber = new HashMap<Product, Integer>();
         buyTwoGetOneProductListWithNumber.put(createProduct(2.0), 1);
         buyTwoGetOneProductListWithNumber.put(createProduct(3.0), 2);
-        buyTwoGetOneProductListWithNumber.put(createProduct(4.0), 3);
         buyTwoGetOneProductListWithNumber.put(createProduct(2.0), 4);
 
         double price = calculatorWithBuyTwoGetOneProduct.calculate(buyTwoGetOneProductListWithNumber);
 
-        assertEquals(22.0, price, 0.01);
+        assertEquals(14.0, price, 0.01);
     }
 
     @Test
-    public void testCalculatePriceWithProductNumberIsThreeMultiple() throws Exception {
+    public void testCalculatePriceWithSingleProductAndProductNumberIsThreeMultiple() throws Exception {
         HashMap<Product, Integer> buyTwoGetOneProductListWithNumber = new HashMap<Product, Integer>();
-        buyTwoGetOneProductListWithNumber.put(createProduct(2.0), 1);
-        buyTwoGetOneProductListWithNumber.put(createProduct(3.0), 2);
         buyTwoGetOneProductListWithNumber.put(createProduct(4.0), 3);
 
         double price = calculatorWithBuyTwoGetOneProduct.calculate(buyTwoGetOneProductListWithNumber);
 
-        assertEquals(16.0, price, 0.01);
+        assertEquals(8.0, price, 0.01);
     }
 
     @Test
-    public void testCalculatePriceWithProductNumberIsNotThreeMultiple() throws Exception {
+    public void testCalculatePriceWithMultipleProductAndProductNumberIsThreeMultiple() throws Exception {
         HashMap<Product, Integer> buyTwoGetOneProductListWithNumber = new HashMap<Product, Integer>();
-        buyTwoGetOneProductListWithNumber.put(createProduct(2.0), 1);
-        buyTwoGetOneProductListWithNumber.put(createProduct(3.0), 2);
+        buyTwoGetOneProductListWithNumber.put(createProduct(2.0), 6);
+        buyTwoGetOneProductListWithNumber.put(createProduct(3.0), 3);
+        buyTwoGetOneProductListWithNumber.put(createProduct(4.0), 3);
 
         double price = calculatorWithBuyTwoGetOneProduct.calculate(buyTwoGetOneProductListWithNumber);
 
-        assertEquals(8.0, price, 0.01);
+        assertEquals(22.0, price, 0.01);
     }
 
     private Product createProduct(double price) {
